58b9ffd078a8e30c7ebd0404575449523874ceda
Fedor Zubarev <zubarevf@gmail.com>
2022-04-03 12:51:23 +0300

    решена задача 2 Скрыть текст по нажатию кнопки модуль 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+let button = document.body.querySelector('.toggle-text-button');
+
+button.addEventListener('click', function(event){
+  let divItem = document.getElementById('text');
+  if (divItem.hidden) {
+    divItem.hidden = false;
+  } else {
+    divItem.hidden = true;
+  };
+});
 }
