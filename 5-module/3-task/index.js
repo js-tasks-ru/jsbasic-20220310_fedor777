50b916d5c6ea842a543d833631cfcc67e1016553
Fedor Zubarev <zubarevf@gmail.com>
2022-04-05 23:24:42 +0300

    решена задача №3 начальная карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,47 @@
 function initCarousel() {
-  // ваш код...
+
+  let carouselBlock = document.body.querySelector('.carousel__inner');
+  let rightArrow = document.body.querySelector('.carousel__arrow_right');
+  let leftArrow = document.body.querySelector('.carousel__arrow_left');
+  let numberOfSlides = 4;
+  let currentSlideNumber = 0;
+  let carouselSlideWidth = carouselBlock.offsetWidth;
+
+  arrowHide();
+
+  rightArrow.addEventListener ('click', function(event) {
+    slideIncrement();
+    slideChange();
+    arrowHide();
+  });
+  leftArrow.addEventListener ('click', function(event) {
+    slideDecrement();
+    slideChange();
+    arrowHide();
+  });
+
+  function slideIncrement () {
+    currentSlideNumber += 1;
+  };
+
+  function slideDecrement () {
+    currentSlideNumber -= 1;
+  };
+
+  function slideChange () {
+    let transformMove = - currentSlideNumber * carouselSlideWidth;
+    carouselBlock.style.transform = `translateX(${transformMove}px)`;
+  };
+
+  function arrowHide () {
+    if (currentSlideNumber === 0) {
+    leftArrow.style.display = 'none';  
+    } else if (currentSlideNumber === (numberOfSlides -1) ) {
+      rightArrow.style.display = 'none';
+    } else {
+      leftArrow.style.display = '';
+      rightArrow.style.display = '';
+    };
+
+  };
 }
