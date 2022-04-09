a11422d60d053a2b6b33591584b4fd1caaca4379
Fedor Zubarev <zubarevf@gmail.com>
2022-04-09 11:31:12 +0300

    решена задача №1 удаление строки из таблицы Модуль 6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,47 @@
  */
 export default class UserTable {
   constructor(rows) {
-  }
-}
+    console.log ('Привет');
+
+    this.elem = document.createElement('table');
+    console.log(this.elem);
+
+    this.elem.innerHTML = 
+    ` <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+          </tr>
+      </thead>
+    `;
+
+    let tableInnerHtml = rows.map (row => {
+      let tableCellswithData = Object.values (row)
+        .map(tdDataValue => `<td>${tdDataValue}</td>`).join('');
+      return `
+          <tr>
+            ${tableCellswithData}
+            <td><button>X</button></td>
+          </tr>
+      `;
+    }).join('');
+
+    this.elem.innerHTML += `
+    <tbody>
+      ${tableInnerHtml}
+    <tbody>
+    `;
+    this.elem.addEventListener('click', (event) => this.xButtonClicked(event));
+
+  };
+
+  xButtonClicked (event){
+    if (event.target.tagName != 'BUTTON')
+      return;
+ 
+    let tr = event.target.closest('tr');
+    tr.remove();
+  };
+};
