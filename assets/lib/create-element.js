68ea2afc9c72b3fd2249764a1b38f6d2da380b46
Fedor Zubarev <zubarevf@gmail.com>
2022-04-13 22:45:29 +0300

    решена задача №2 карточка товара Модуль 6

--- assets/lib/create-element.js
+++ assets/lib/create-element.js
@@ -1,6 +1,18 @@
-// doesn't work for td and some other elements that may not be placed into <div>
-export default function(html) {
-  const div = document.createElement('div');
-  div.innerHTML = html;
-  return div.firstElementChild;
-};
+export default function(price, imageLink, productName) {
+const div = document.createElement('div');
+div.classList.add('card');
+div.innerHTML = `
+    <div class="card__top">
+        <img src="${imageLink}" class="card__image" alt="product">
+        <span class="card__price">€${price}</span>
+    </div>
+    <div class="card__body">
+        <div class="card__title">${productName}</div>
+        <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+    </div>
+`
+return div;
+
+}
